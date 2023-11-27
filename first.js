0b3e7aa1e5bf52e596915f511cee8cdaa22c6385
dashaa1204 <dashaa1204@gmail.com>
2023-11-27 12:45:37 +0800

    first-commit

---
+++ first.js
@@ -0,0 +1,107 @@
+// let firstName = "dashaa";
+// let myAge = 30;
+// console.log(firstName);
+// console.log(myAge);
+
+// let num1 = 10;
+// let num2 = 20;
+
+// console.log(num1 + num2);
+// console.log(num1 * num2);
+// console.log(num1 / num2);
+// console.log(num1 - num2);
+
+// let sum = num1 + num2;
+// let product = num1 * num2;
+// let division = num1 / num2;
+// let subtraction = num1 - num2;
+
+// //y = x*x + 2*x +1;
+// let x = 5;
+// let answer = x * x + 2 * x + 1;
+// console.log(answer);
+
+// let number1 = 10;
+// let number2 = 20;
+
+// console.log(number1 + number2);
+
+// let x = 15;
+// let y = 20;
+// if (x > y) {
+//   console.log("x is bigger");
+// } else {
+//   console.log("y is bigger");
+// }
+
+// let myAge = 30;
+// if (myAge < 21) {
+//   console.log("no bar");
+// } else {
+//   console.log("bar or");
+// }
+
+// let myBirthYear = 1666;
+// let Age = 2023 - myBirthYear;
+
+// console.log(Age);
+
+// let myAge = 16;
+// if (myAge < 18) {
+//   console.log("you are not allowed to drive");
+// } else {
+//   console.log("you are allowed to drive");
+// }
+
+// let myAge = 30;
+// console.log("my age =", myAge);
+
+// Data type
+
+// let number1 = 15; //numbertype
+// let myString = "15"; //string type
+// let myBool = false; //boolean type
+
+// let day = 9;
+// if (day == 1) {
+//   console.log("Monday");
+// } else if (day == 2) {
+//   console.log("Tuesday");
+// } else if (day == 3) {
+//   console.log("Wednesday");
+// } else if (day == 4) {
+//   console.log("Thursday");
+// } else if (day == 5) {
+//   console.log("Friday");
+// } else if (day == 6) {
+//   console.log("Saturday");
+// } else if (day == 7) {
+//   console.log("Sunday");
+// } else console.log("Error 404");
+
+let days = 333;
+if (days < 32) {
+  console.log("January");
+} else if (days < 60) {
+  console.log("February");
+} else if (days < 91) {
+  console.log("March");
+} else if (days < 121) {
+  console.log("April");
+} else if (days < 151) {
+  console.log("May");
+} else if (days < 182) {
+  console.log("June");
+} else if (days < 213) {
+  console.log("July");
+} else if (days < 242) {
+  console.log("August");
+} else if (days < 274) {
+  console.log("September");
+} else if (days < 305) {
+  console.log("Octeber");
+} else if (days < 336) {
+  console.log("November");
+} else if (days < 366) {
+  console.log("December");
+} else console.log("Error");
