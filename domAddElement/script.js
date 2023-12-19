fb913ec357408522d7672b40d4ded1a9aa547eec
Дашноров дамдиндорж <23LP9512@G301-CC0132.local>
2023-12-19 12:34:49 +0800

    class

--- domAddElement/script.js
+++ domAddElement/script.js
@@ -1,6 +1,5 @@
 function add() {
   const addBox = document.getElementById("box").innerHTML;
-  console.log(addBox);
 
   document.getElementById("box").innerHTML =
     document.getElementById("box").innerHTML +
--- domCheckNumber/srcipt.js
+++ domCheckNumber/srcipt.js
@@ -1,1 +1,7 @@
-Number(document.getElementById("num1").value);
+let num1=Number(document.getElementById("num1").value);
+let num2=Number(document.getElementById("num2").value);
+
+function check(){
+    if(num1+num2==Number(document.getElementById("inputBox").value)) {
+        alert("zuw")
+    }}
